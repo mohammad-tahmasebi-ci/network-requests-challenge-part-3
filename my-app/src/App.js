93649873f4a2f0951b783c926860f5b646024e13
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-08-31 23:02:26 +0100

    Update App.js
    update App.js with the required entries to reference ContentAPIHooks.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import ContentAPIHooks from './components/ContentAPIHooks';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       <Sidebar />
+      <ContentAPIHooks />
     </div>
   );
 }
